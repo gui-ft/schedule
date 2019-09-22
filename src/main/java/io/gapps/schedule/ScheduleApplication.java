f624db3e9c8a67ed53a45a6cfce1ba6b31cab120
Guilherme F <guilhermealexsandro77@gmail.com>
2019-09-22 01:26:42 -0300

    first commit

---
+++ src/main/java/io/gapps/schedule/ScheduleApplication.java
@@ -0,0 +1,13 @@
+package io.gapps.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleApplication.class, args);
+	}
+
+}
